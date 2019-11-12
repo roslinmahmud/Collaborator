e7c4d97a5eaadcf906a32292f4ea6a38a1bb937e
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2019-11-12 22:40:09 +0600

    Add project files.

---
+++ Collaborator/DBConnection.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Windows;
+using System.Net;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace Collaborator
+{
+    //Singleton design pattern used
+    class DBConnection
+    {
+        private MainWindow mainWindow = null;
+        private static DBConnection instance = null;
+        private MySqlConnection connection = null;
+        private Ping ping = null;
+        private string connectionString = "Server=remotemysql.com;Port=3306;Database=5oj1fjtwef;Uid=5oj1fjtwef;Pwd=MJ8T2gcsFB;";
+
+        private DBConnection()
+        {
+            ping = new Ping();
+        }
+        public MainWindow MainWindow
+        {
+            set
+            {
+                mainWindow = value;
+            }
+        }
+        public static DBConnection Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DBConnection();
+                    return instance;
+                }
+                else
+                {
+                    return instance;
+                }
+            }
+        }
+
+        public MySqlConnection Connection
+        {
+            get
+            {
+                if (connection == null)
+                {
+                    try
+                    {
+                        connection = new MySqlConnection(connectionString);
+                        connection.Open();
+                    }
+                    catch (Exception e)
+                    {
+                        connection = null;
+                        MessageBox.Show(e.Message, this.ToString() + " Connection Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                return connection;
+            }
+            set
+            {
+                connection = value;
+            }
+        }
+
+        public void Connect()
+        {
+            if (connection == null)
+            {
+                try
+                {
+                    connection = new MySqlConnection(connectionString);
+                    connection.Open();
+                }
+                catch (Exception e)
+                {
+                    connection = null;
+                    MessageBox.Show(e.Message, this.ToString() + " Connect() Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            return;
+        }
+
+        public bool IsConnect()
+        {
+            return !(connection == null);
+        }
+        
+        public void CheckInternetConnection()
+        {
+            while (true)
+            {
+                try
+                {
+                    if(ping.Send("www.google.com.mx").Status == IPStatus.Success)
+                    {
+                        if (mainWindow.ErrorMessage.IsVisible)
+                        {
+                            mainWindow.Dispatcher.Invoke(() =>
+                            {
+                                mainWindow.ErrorMessage.Visibility = Visibility.Collapsed;
+                            });
+
+                        }
+                        if(!IsConnect())
+                        {
+                            Connect();
+                        }
+                    }
+                    
+                }
+                catch(Exception e)
+                {
+                    mainWindow.Dispatcher.Invoke(() =>
+                    {
+                        mainWindow.ErrorMessage.Visibility = Visibility.Visible;
+                    });
+                    DBConnection.Instance.Connection = null;
+                }
+                finally
+                {
+                    Thread.Sleep(3000);
+                    
+                }
+            }
+        }
+
+    }
+}
---
+++ Collaborator/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Collaborator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DBConnection.Instance.MainWindow = this;
+            Thread DBConnect = new Thread(new ThreadStart(DBConnection.Instance.CheckInternetConnection));
+            DBConnect.Start();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void PackIconMaterial_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void PackIconMaterial_MouseEnter(object sender, MouseEventArgs e)
+        {
+            closeIcon.Foreground = Brushes.Red;
+        }
+
+        private void closeIcon_MouseLeave(object sender, MouseEventArgs e)
+        {
+            closeIcon.Foreground = Brushes.White;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT USERNAME, PASSWORD FROM USER";
+            try
+            {
+                DBConnection.Instance.Connect();
+                using(MySqlCommand mySqlCommand = new MySqlCommand(sql, DBConnection.Instance.Connection)){
+                    MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+                    MessageBox.Show("connected");
+                    while (mySqlDataReader.Read())
+                    {
+                        MessageBox.Show(mySqlDataReader.GetString(1));
+                    }
+                }
+
+            }
+            catch (Exception ec)
+            {
+                DBConnection.Instance.Connection = null;
+                MessageBox.Show(this.ToString() + " Exception: " + ec.Message);
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RegisterBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TopLoginPanel.Visibility = Visibility.Collapsed;
+            BottomLoginPanel.Visibility = Visibility.Collapsed;
+            TopRegisterPanel.Visibility = Visibility.Visible;
+            FullNamePanel.Visibility = Visibility.Visible;
+            BottomRegisterPanel.Visibility = Visibility.Visible;
+        }
+
+        private void LoginBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TopRegisterPanel.Visibility = Visibility.Collapsed;
+            FullNamePanel.Visibility = Visibility.Collapsed;
+            BottomRegisterPanel.Visibility = Visibility.Collapsed;
+            TopLoginPanel.Visibility = Visibility.Visible;
+            BottomLoginPanel.Visibility = Visibility.Visible;
+        }
+        public void ShowInternetConnectionError()
+        {
+
+        }
+    }
+}
