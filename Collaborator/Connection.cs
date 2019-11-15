6453faac4333f4c50ea9153818ec2362df0fc2df
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2019-11-15 10:42:03 +0600

    Connection.cs, Register.cs added

---
+++ Collaborator/Connection.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+
+namespace Collaborator
+{
+    class Connection
+    {
+        private static Connection connection = null;
+        private MainWindow mainWindow = null;
+        private Ping ping = null;
+        private Connection()
+        {
+            ping = new Ping();
+        }
+        public static Connection Instance
+        {
+            get
+            {
+                if(connection == null)
+                {
+                    connection = new Connection();
+                }
+                return connection;
+            }
+        }
+        public MainWindow MainWindow
+        {
+            set
+            {
+                mainWindow = value;
+            }
+        }
+        public void CheckInternetConnection()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (ping.Send("www.google.com.mx").Status == IPStatus.Success)
+                    {
+                        if (mainWindow.ErrorMessage.IsVisible)
+                        {
+                            mainWindow.Dispatcher.Invoke(() =>
+                            {
+                                mainWindow.ErrorMessage.Visibility = Visibility.Collapsed;
+                            });
+
+                        }
+                        if (!DBConnection.Instance.IsConnect())
+                        {
+                            DBConnection.Instance.Connect();
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    mainWindow.Dispatcher.Invoke(() =>
+                    {
+                        mainWindow.ErrorMessage.Visibility = Visibility.Visible;
+                    });
+                    DBConnection.Instance.Connection = null;
+                }
+                finally
+                {
+                    Thread.Sleep(3000);
+
+                }
+            }
+        }
+        public static String HostIP
+        {
+            get
+            {
+                foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (ni.GetIPProperties().GatewayAddresses.FirstOrDefault() != null)
+                    {
+                        if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                        {
+                            foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                            {
+                                if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                                {
+                                    return (ip.Address.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+                return "127.0.0.1";
+            }
+        }
+    }
+}
--- Collaborator/Register.cs
+++ Collaborator/Register.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows;
 
 namespace Collaborator
 {
     class Register
     {
+        private string username;
+        private string fullname;
+        private string password;
+        private string photo_path;
+        private string ip;
+
+        public Register()
+        {
+
+        }
+        public Register(string username, string fullname, string password)
+        {
+            this.username = username;
+            this.fullname = fullname;
+            this.password = password;
+            this.photo_path = "Avatar/user.png";
+            this.ip = Connection.HostIP;
+        }  
+
+        public bool Perform()
+        {
+            DBConnection.Instance.Connect();
+            string query = "INSERT INTO USER (USERNAME, NAME, PASSWORD, PHOTO_PATH, IP) VALUES ('"+username+"','"+fullname+"','"+password+"','"+photo_path+"','"+ip+"');";
+            try
+            {
+                using (MySqlCommand mySqlCommand = new MySqlCommand(query, DBConnection.Instance.Connection))
+                {
+                    MessageBox.Show(mySqlCommand.ExecuteNonQuery().ToString());
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message, this.ToString() + " Perform() Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
     }
 }
