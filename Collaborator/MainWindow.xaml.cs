b89a17b23ad9dea055345a334f1033df978463b8
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2020-03-06 07:26:00 +0600

    BL Layer added

--- Collaborator/MainWindow.xaml.cs
+++ Collaborator/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace Collaborator
 {
@@ -24,7 +15,6 @@ namespace Collaborator
         public MainWindow()
         {
             InitializeComponent();
-
             Connection.Instance.MainWindow = this;
 
             Thread Connect = new Thread(new ThreadStart(Connection.Instance.CheckInternetConnection));
